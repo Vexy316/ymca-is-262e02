8e839063e1dae9349529f9b172a29be79bb051be
Vexy316 <patrick.fritschle@gmail.com>
2026-03-14 08:26:17 -0600

    Add files via upload

---
+++ IS262E02/src/Application262E02/Rectangulo262E02.cs
@@ -0,0 +1,17 @@
+﻿public class Rectangulo262E02
+{
+    public static double Perimetro(double baseRect, double alturaRect)
+    {
+        return (baseRect * 2) + (alturaRect * 2);
+    }
+
+    public static double Area(double baseRect, double alturaRect)
+    {
+        return baseRect * alturaRect;
+    }
+
+    public static double Volumen(double baseRect, double alturaRect, double profundidad)
+    {
+        return baseRect * alturaRect * profundidad;
+    }
+}
---
+++ IS262E02/src/ConsoleApp262E02/Program.cs
@@ -0,0 +1,11 @@
+﻿double baseRect = 5;
+double alturaRect = 8;
+double profundidad = 10;
+
+double perimetro = Rectangulo262E02.Perimetro(baseRect, alturaRect);
+double area = Rectangulo262E02.Area(baseRect, alturaRect);
+double volumen = Rectangulo262E02.Volumen(baseRect, alturaRect, profundidad);
+
+Console.WriteLine($"El perímetro de la base de un prisma rectangular de {baseRect}cm por {alturaRect}cm es {perimetro}cm");
+Console.WriteLine($"El área de la base de un prisma rectangular de {baseRect}cm por {alturaRect}cm es {area}cm2");
+Console.WriteLine($"El volumen de un prisma rectangular de {baseRect}cm por {alturaRect}cm y {profundidad}cm de profundidad es {volumen}cm3");
